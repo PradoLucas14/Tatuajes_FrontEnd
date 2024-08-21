c08af8704f2e3e2daf9367927f2172c064af7926
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-08-20 22:49:41 -0300

    se creo la estructura inicial del apartado de artistas del estudio

--- EstudioTatuajes/src/pages/home/Home.jsx
+++ EstudioTatuajes/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './homeComponents/Banner'
 import About from './homeComponents/About'
+import Artist from './homeComponents/Artist'
 
 function Home() {
   return (
     <div>
       <Banner/>
       <About/>
+      <Artist/>
     </div>
   )
 }
---
+++ EstudioTatuajes/src/pages/home/homeComponents/Artist.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Artist.css"
+
+function Artist() {
+    return (
+        <div className='Artists'>
+            <div className="artistTitulo">
+                <h4>NUESTROS TATUADORES</h4>
+            </div>
+            <div className='descripArtist'>
+                <p>
+                    En Black Magic, contamos con un equipo de tatuadores apasionados y expertos en diversas técnicas artísticas. 
+                    Cada miembro se especializa en estilos únicos que van desde la elegancia del minimalismo y la sutileza de la 
+                    inspiración oriental, hasta la intensidad del estilo gótico y la precisión en el arte del blackout. Nuestros 
+                    tatuadores combinan creatividad y técnica para transformar ideas en obras de arte sobre la piel, 
+                    siempre garantizando un trabajo de la más alta calidad.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Artist
